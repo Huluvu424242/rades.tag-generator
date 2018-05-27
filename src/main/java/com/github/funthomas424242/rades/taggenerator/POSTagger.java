4376935a8a718a9b9431357028c711179e47b4e4
FunThomas424242 <funthomas424242@gmail.com>
2018-05-27 14:16:27 +0200

    Taggerfile wird im Konstruktor übergeben

--- src/main/java/com/github/funthomas424242/rades/taggenerator/POSTagger.java
+++ src/main/java/com/github/funthomas424242/rades/taggenerator/POSTagger.java
@@ -3,16 +3,23 @@ package com.github.funthomas424242.rades.taggenerator;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class POSTagger {
 
+    final Path modelFilepath;
+
+    public POSTagger(final Path modelFilepath){
+        this.modelFilepath=modelFilepath;
+    }
+
 
     public List<TokenDescriptionAccessor> createTokenDescriptions(String text) throws IOException, ClassNotFoundException {
 
-        final MaxentTagger maxentTagger = new MaxentTagger("german-fast.tagger");
+        final MaxentTagger maxentTagger = new MaxentTagger(modelFilepath.toString());
         ;
         final String tagLine = maxentTagger.tagString(text);
         final String[] tagDescription = tagLine.split("\\s+");
--- src/test/java/com/github/funthomas424242/rades/taggenerator/POSTaggerTest.java
+++ src/test/java/com/github/funthomas424242/rades/taggenerator/POSTaggerTest.java
@@ -3,27 +3,35 @@ package com.github.funthomas424242.rades.taggenerator;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class POSTaggerTest {
 
     @Test
-    public void tag() throws IOException, ClassNotFoundException {
-        final POSTagger posTagger = new POSTagger();
-        final List<TokenDescriptionAccessor> tokens = posTagger.createTokenDescriptions("In diesem Programm geht es um die Programmiersprache Java. Außerdem wollen " +
-                "wir uns mit Sprachanalyse befassen. Die Sprache wird in Token zerlegt und es wird die" +
-                "Wortform (Verb, Substantiv, ...) bestimmt. Dabei werden die Stopp Wörter ignoriert." +
-                "Anschließend wird durchgezählt. Die 5 Wörter mit den höchsten Zahlen sollten sich " +
-                "zum automatischen Taggen des Textes als Schlagwörter eignen.");
-        assertEquals("In",tokens.get(0).getWort());
-        assertEquals("APPR",tokens.get(0).getWortArt());
-        assertEquals("Programm",tokens.get(2).getWort());
-        assertEquals("NN",tokens.get(2).getWortArt());
-        assertEquals("geht",tokens.get(3).getWort());
-        assertEquals("VVFIN",tokens.get(3).getWortArt());
-
+    public void tag() {
+        final Path path = Paths.get("model/german-fast.tagger");
+        final POSTagger posTagger = new POSTagger(path);
+        final List<TokenDescriptionAccessor> tokens;
+        try {
+            tokens = posTagger.createTokenDescriptions("In diesem Programm geht es um die Programmiersprache Java. Außerdem wollen " +
+                    "wir uns mit Sprachanalyse befassen. Die Sprache wird in Token zerlegt und es wird die" +
+                    "Wortform (Verb, Substantiv, ...) bestimmt. Dabei werden die Stopp Wörter ignoriert." +
+                    "Anschließend wird durchgezählt. Die 5 Wörter mit den höchsten Zahlen sollten sich " +
+                    "zum automatischen Taggen des Textes als Schlagwörter eignen.");
+            assertEquals("In", tokens.get(0).getWort());
+            assertEquals("APPR", tokens.get(0).getWortArt());
+            assertEquals("Programm", tokens.get(2).getWort());
+            assertEquals("NN", tokens.get(2).getWortArt());
+            assertEquals("geht", tokens.get(3).getWort());
+            assertEquals("VVFIN", tokens.get(3).getWortArt());
+        } catch (IOException | ClassNotFoundException e) {
+            fail();
+        }
     }
 }
 
