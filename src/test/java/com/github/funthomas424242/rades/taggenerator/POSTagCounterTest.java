0bdf28398745446ccb9325c460e7b61c90242b7a
FunThomas424242 <funthomas424242@gmail.com>
2018-05-27 19:53:17 +0200

    Infrastruktur added

--- src/test/java/com/github/funthomas424242/rades/taggenerator/POSTagCounterTest.java
+++ src/test/java/com/github/funthomas424242/rades/taggenerator/POSTagCounterTest.java
@@ -1,9 +1,30 @@
 package com.github.funthomas424242.rades.taggenerator;
 
+/*-
+ * #%L
+ * rades.tag-generator
+ * %%
+ * Copyright (C) 2018 PIUG
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import com.google.common.collect.Multiset;
 import edu.stanford.nlp.ling.TaggedWord;
 import org.junit.jupiter.api.Test;
-import sun.nio.cs.UTF_32;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -20,17 +41,16 @@ class POSTagCounterTest {
         final Path modelFilepath = Paths.get("model/german-fast.tagger");
         final Charset charset = StandardCharsets.UTF_8;
         final String posTagCategories = "NN|NNP|NNS|NNPS";
-        final POSTagCounter tagCounter = new POSTagCounter(modelFilepath,charset,posTagCategories);
+        final POSTagCounter tagCounter = new POSTagCounter(modelFilepath, charset, posTagCategories);
 
-        final Path textFile = Paths.get("src/main/resources","Der_Prozess.txt");
+        final Path textFile = Paths.get("src/main/resources", "Der_Prozess.txt");
         try {
-            final Multiset<TaggedWord> keywords= tagCounter.parse(textFile);
-            tagCounter.printSortedNounsCount(keywords);
+            final Multiset<TaggedWord> keywords = tagCounter.parse(textFile);
+            // tagCounter.printSortedNounsCount(keywords);
         } catch (Exception e) {
             e.printStackTrace();
             fail();
         }
-
 
 
     }
