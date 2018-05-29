6c9b95c13b1429be24948d0904671acf137efa76
FunThomas424242 <funthomas424242@gmail.com>
2018-05-29 22:59:11 +0200

    POSTagger Beispiel verbessert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/taggenerator/POSTaggerTest.java
+++ src/test/java/com/github/funthomas424242/rades/taggenerator/POSTaggerTest.java
@@ -10,12 +10,12 @@ package com.github.funthomas424242.rades.taggenerator;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -23,8 +23,12 @@ package com.github.funthomas424242.rades.taggenerator;
  */
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -34,23 +38,25 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class POSTaggerTest {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(POSTaggerTest.class);
+
     @Test
     public void tag() {
         final Path path = Paths.get("model/german-fast.tagger");
         final POSTagger posTagger = new POSTagger(path);
-        final List<TokenDescriptionAccessor> tokens;
         try {
-            tokens = posTagger.createTokenDescriptions("In diesem Programm geht es um die Programmiersprache Java. Außerdem wollen " +
-                    "wir uns mit Sprachanalyse befassen. Die Sprache wird in Token zerlegt und es wird die" +
-                    "Wortform (Verb, Substantiv, ...) bestimmt. Dabei werden die Stopp Wörter ignoriert." +
-                    "Anschließend wird durchgezählt. Die 5 Wörter mit den höchsten Zahlen sollten sich " +
-                    "zum automatischen Taggen des Textes als Schlagwörter eignen.");
-            assertEquals("In", tokens.get(0).getWort());
-            assertEquals("APPR", tokens.get(0).getWortArt());
-            assertEquals("Programm", tokens.get(2).getWort());
-            assertEquals("NN", tokens.get(2).getWortArt());
-            assertEquals("geht", tokens.get(3).getWort());
-            assertEquals("VVFIN", tokens.get(3).getWortArt());
+            final Path textPath = Paths.get("src/test/resources", "Der_Prozess.txt");
+            final byte[] encoded = Files.readAllBytes(textPath);
+            final String text = new String(encoded, StandardCharsets.UTF_8).substring(341,490);
+
+            final List<TokenDescriptionAccessor> tokens = posTagger.createTokenDescriptions(text);
+            assertEquals("Jemand", tokens.get(0).getWort());
+            assertEquals("NE", tokens.get(0).getWortArt());
+            assertEquals("verleumdet", tokens.get(4).getWort());
+            assertEquals("VVPP", tokens.get(4).getWortArt());
+            assertEquals("Köchin", tokens.get(23).getWort());
+            assertEquals("NN", tokens.get(23).getWortArt());
+            tokens.forEach(token -> LOGGER.debug(token.getWort() + "_" + token.getWortArt()));
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             fail();
